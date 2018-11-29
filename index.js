f4cdb863c17f9aa0842b27543951e244680f6a49
Agliullin Dmitry <dmitry.agli@gmail.com>
2018-11-29 15:51:44 +0500

    Add expressjs

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const request = require('superagent');
 const cheerio = require('cheerio');
 const nodemailer = require('nodemailer');
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || '3000';
 
 const mail_list = ['dmitry.agli@gmail.com'];
 
 let first_sm_marker;
 let second_sm_marker;
+const empty_data = 'Данных нет, информация обновляется после 06:00 и после 11:00';
+
+app.get('/', (req, res) => {
+  
+  request
+    .get('https://sitv.ru/actirovka/')
+    .end((err, res) => {
+      const $ = cheerio.load(res.text);
+      const date = $('.activ').children().eq(0).text();
+      const first_sm = $('.activ').children().eq(1).text();
+      const first_sm_data = $('.activ').children().eq(2).text();
+      const second_sm = $('.activ').children().eq(3).text();
+      const second_sm_data = $('.activ').children().eq(4).text();
+      
+      if (first_sm_data !== empty_data) {
+        if (first_sm_marker !== date) {
+          first_sm_marker = date;
+          sendMails(date,fisrt_sm,first_sm_data);
+        }
+      }
+      
+      if (second_sm_data !== empty_data) {
+        if (second_sm_marker !== date) {
+          second_sm_marker = date;
+          sendMails(date,second_sm,second_sm_data);
+        }
+      }
+  
+    });
+    
+  res.send('200 OK');
+
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 function sendMails(date,sm,sm_data) {
   const transporter = nodemailer.createTransport({
@@ -38,36 +77,3 @@ function sendMails(date,sm,sm_data) {
     }
   });
 };
-
-
-setInterval(function() {
-
-  request
-    .get('https://sitv.ru/actirovka/')
-    .end((err, res) => {
-      const $ = cheerio.load(res.text);
-      const date = $('.activ').children().eq(0).text();
-      const first_sm = $('.activ').children().eq(1).text();
-      const first_sm_data = $('.activ').children().eq(2).text();
-      const second_sm = $('.activ').children().eq(3).text();
-      const second_sm_data = $('.activ').children().eq(4).text();
-      const empty_data = 'Данных нет, информация обновляется после 06:00 и после 11:00';
-
-      if (first_sm_data !== empty_data) {
-        if (first_sm_marker !== date) {
-          first_sm_marker = date;
-          sendMails(date,fisrt_sm,first_sm_data);
-        }
-      }
-      
-      if (second_sm_data !== empty_data) {
-        if (second_sm_marker !== date) {
-          second_sm_marker = date;
-          sendMails(date,second_sm,second_sm_data);
-        }
-      }
-  
-  });
-
-}, 60000);
-
